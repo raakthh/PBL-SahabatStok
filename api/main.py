beab932e79f309b03cd5c615b62acf8b61e89d83
raakthh <salmaxyz0@gmail.com>
2026-05-26 22:38:40 +0700

    Update main.py

--- api/main.py
+++ api/main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "API running"}
+
+@app.get("/predict")
+def predict():
+    return {
+        "prediction": 150,
+        "status": "success"
+    }
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
